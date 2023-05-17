2ccb09ad53dd01f3ad32408a3cb4671072b70ef4
Lauren Giordano <laurengiordano94@gmail.com>
2023-05-17 10:08:26 +1000

    done with lab

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,32 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+
+
+doc = Nokogiri::HTML(URI.open("https://flatironschool.com/"))
+
+ puts "#{doc.css(".site-logo").text.strip}: "
+
+ courses = doc.css(".heading-25-extrabold.color-black")
+
+#  puts courses[0].attributes
+
+ course_info = doc.css(".column-item.col-lg-3.col-sm-6 p")
+
+#  puts course_info[2].text.strip
+ 
+
+#  courses.each do |course|
+#     puts course.text.strip
+#  end
+
+
+    i = 0
+    x = 0
+    while i < 4 && x < 8 do
+        puts "#{courses[i].text.strip} - #{course_info[x].text.strip}"
+        i += 1
+        x += 2
+    end
+
+
